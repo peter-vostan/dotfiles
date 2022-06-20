335a0f6d9092b41344ffbc2a6b559cb4e815ce5c
Peter Vostan <peter.vostan@gmail.com>
2022-06-20 22:21:01 +0800

    nvm -> fnm, starship

--- brew/install-packages.sh
+++ brew/install-packages.sh
@@ -7,14 +7,15 @@ brew install --cask rectangle
 brew install --cask drawio
 brew install --cask kap
 
+brew install starship
 brew install diff-so-fancy
 brew install direnv
-brew install navi
+brew install navi                           # cheatsheets
 brew install zsh-autosuggestions
 brew install zsh-syntax-highlighting
 
 brew install rust
-brew install nvm
+brew install fnm                            # Faster version of npm
 
 brew install awscli
 brew install azure-cli

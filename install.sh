c2cfaadf218bb9f51f6e6503aa9dc51d7e49c5bf
Peter Vostan <peter.vostan@gmail.com>
2022-08-10 10:28:47 +0800

    minor change

--- install.sh
+++ install.sh
@@ -100,7 +100,6 @@ git config --global core.excludesfile ~/.gitignore_global
 if ! git config --global alias.hack > /dev/null; then
     echo 'Adding git-town aliases'
     git-town alias true
-    git-town new-branch-push-flag true
 fi
 
 # OS specific config

4e89d17e0f29fbbf1433ec7b83dc8bd33f55129a
Peter Vostan <peter.vostan@gmail.com>
2024-06-07 21:50:20 +0800

    update macos defaults

--- darwin/system-defaults.nix
+++ darwin/system-defaults.nix
@@ -1,9 +1,18 @@
 { config, pkgs, ... }:
 
 {
+  # https://github.com/LnL7/nix-darwin/tree/master/modules/system/defaults
   system.defaults = {
+    CustomUserPreferences = {
+      "~/Library/Preferences/ByHost/com.apple.controlcenter.plist" = {
+        "Bluetooth" = 18;
+        "BatteryShowPercentage" = 1;
+      };
+    };
+
     dock = {
       mineffect = "scale";
+      minimize-to-application = true; # Minimize windows into their application's icon, might need to be manually applied if this isn't working
       tilesize = 36;
       show-recents = false;
       autohide = true;
@@ -12,11 +21,23 @@
     };
 
     finder = {
+      FXDefaultSearchScope = "SCcf"; # Search current folder
+      FXPreferredViewStyle = "Nlsv"; # List view
       ShowPathbar = true;
       ShowStatusBar = true;
     };
 
+    menuExtraClock = {
+      ShowDayOfWeek = false;
+      ShowSeconds = false;
+    };
+
     screencapture.type = "jpg";
+
+    trackpad = {
+      Clicking = true; # Enable tap to click, might need to be manually applied if this isn't working
+      # Manually turn "Look up and Data Detectors" off in System Preferences > Trackpad > Point & Click
+    };
 
     NSGlobalDomain = {
       AppleShowAllExtensions = true;

7547d8244f4ae1054590d3fa1ce3825d0998e0e4
Peter Vostan <peter.vostan@gmail.com>
2024-12-07 21:26:48 +0800

    remove dns config

--- hosts/macbook-air/configuration.nix
+++ hosts/macbook-air/configuration.nix
@@ -8,13 +8,6 @@
   security.pam.enableSudoTouchIdAuth = true;
 
   networking.hostName = "peters-macbook-air";
-  networking.knownNetworkServices = [ "Wi-Fi" ];
-  networking.dns = [
-    "1.1.1.1"
-    "1.0.0.1"
-    "2606:4700:4700::1111"
-    "2606:4700:4700::1001"
-  ];
 
   users.users.peter = {
     name = "peter";
